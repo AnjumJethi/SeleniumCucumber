0f9fe93037e16691afda7f62d543362c938f6b96
AnjumJethi <anjum.jethi@cloudsufi.com>
2021-12-01 11:51:53 +0530

    First commit

--- src/test/java/com/cucumberSelenium/testDemoSteps.java
+++ src/test/java/com/cucumberSelenium/testDemoSteps.java
@@ -3,20 +3,25 @@ package com.cucumberSelenium;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.testng.Assert;
 
 public class testDemoSteps {
     @Given("account balance is {double}")
     public void accountBalanceIs(double arg0) {
         System.out.println(arg0);
+        Assert.assertTrue(false);
     }
 
     @When("the account is credited with {double}")
     public void theAccountIsCreditedWith(double arg0) {
+
         System.out.println(arg0);
+        Assert.assertTrue(false);
     }
 
     @Then("account should have a balance of {double}")
     public void accountShouldHaveABalanceOf(double arg0) {
+        Assert.assertTrue(true);
         System.out.println(arg0);
     }
 }
