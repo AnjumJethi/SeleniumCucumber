39ab4553bb597b92132c6deec0e57e95c38823ff
AnjumJethi <anjum.jethi@cloudsufi.com>
2021-12-02 12:51:08 +0530

    GitHubAction file updated

--- src/test/java/com/cucumberSelenium/testDemoSteps.java
+++ src/test/java/com/cucumberSelenium/testDemoSteps.java
@@ -9,7 +9,7 @@ public class testDemoSteps {
     @Given("account balance is {double}")
     public void accountBalanceIs(double arg0) {
         System.out.println(arg0);
-        Assert.assertTrue(true);
+        Assert.assertTrue(false);
     }
 
     @When("the account is credited with {double}")
@@ -21,6 +21,24 @@ public class testDemoSteps {
 
     @Then("account should have a balance of {double}")
     public void accountShouldHaveABalanceOf(double arg0) {
+        Assert.assertTrue(true);
+        System.out.println(arg0);
+    }
+
+    @Given("an account balance is {double}")
+    public void anAccountBalanceIs(double arg0) {
+        Assert.assertTrue(true);
+        System.out.println(arg0);
+    }
+
+    @When("the account is debited with {double}")
+    public void theAccountIsDebitedWith(double arg0) {
+        Assert.assertTrue(true);
+        System.out.println(arg0);
+    }
+
+    @Then("account should have a balance of {double} with account holder")
+    public void accountShouldHaveABalanceOfWithAccountHolder(double arg0) {
         Assert.assertTrue(true);
         System.out.println(arg0);
     }
